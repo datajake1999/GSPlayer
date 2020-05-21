2d6a7c7913f5cdbd322253c04e0ed8e8c293f64e
datajake1999 <datajake@gmail.com>
2020-05-21 05:12:51 -0700

    Reindented ResetConfig.c.

--- ResetConfig/ResetConfig.c
+++ ResetConfig/ResetConfig.c
@@ -22,86 +22,86 @@
 
 BOOL RegDelnodeRecurse (HKEY hKeyRoot, LPTSTR lpSubKey)
 {
-    LPTSTR lpEnd;
-    LONG lResult;
-    DWORD dwSize;
-    TCHAR szName[MAX_PATH];
-    HKEY hKey;
-    FILETIME ftWrite;
+	LPTSTR lpEnd;
+	LONG lResult;
+	DWORD dwSize;
+	TCHAR szName[MAX_PATH];
+	HKEY hKey;
+	FILETIME ftWrite;
 
-    // First, see if we can delete the key without having
-    // to recurse.
+	// First, see if we can delete the key without having
+	// to recurse.
 
-    lResult = RegDeleteKey(hKeyRoot, lpSubKey);
+	lResult = RegDeleteKey(hKeyRoot, lpSubKey);
 
-    if (lResult == ERROR_SUCCESS) 
-        return TRUE;
+	if (lResult == ERROR_SUCCESS) 
+	return TRUE;
 
-    lResult = RegOpenKeyEx (hKeyRoot, lpSubKey, 0, KEY_READ, &hKey);
+	lResult = RegOpenKeyEx (hKeyRoot, lpSubKey, 0, KEY_READ, &hKey);
 
-    if (lResult != ERROR_SUCCESS) 
-    {
-        if (lResult == ERROR_FILE_NOT_FOUND) {
-            printf("Key not found.\n");
-            LogFileWrite("Key not found.\n");
-            return TRUE;
-        } 
-        else {
-            printf("Error opening key.\n");
-            LogFileWrite("Error opening key.\n");
-            return FALSE;
-        }
-    }
+	if (lResult != ERROR_SUCCESS) 
+	{
+		if (lResult == ERROR_FILE_NOT_FOUND) {
+			printf("Key not found.\n");
+			LogFileWrite("Key not found.\n");
+			return TRUE;
+		} 
+		else {
+			printf("Error opening key.\n");
+			LogFileWrite("Error opening key.\n");
+			return FALSE;
+		}
+	}
 
-    // Check for an ending slash and add one if it is missing.
+	// Check for an ending slash and add one if it is missing.
 
-    lpEnd = lpSubKey + lstrlen(lpSubKey);
+	lpEnd = lpSubKey + lstrlen(lpSubKey);
 
-    if (*(lpEnd - 1) != TEXT('\\')) 
-    {
-        *lpEnd =  TEXT('\\');
-        lpEnd++;
-        *lpEnd =  TEXT('\0');
-    }
+	if (*(lpEnd - 1) != TEXT('\\')) 
+	{
+		*lpEnd =  TEXT('\\');
+		lpEnd++;
+		*lpEnd =  TEXT('\0');
+	}
 
-    // Enumerate the keys
+	// Enumerate the keys
 
-    dwSize = MAX_PATH;
-    lResult = RegEnumKeyEx(hKey, 0, szName, &dwSize, NULL,
-                           NULL, NULL, &ftWrite);
+	dwSize = MAX_PATH;
+	lResult = RegEnumKeyEx(hKey, 0, szName, &dwSize, NULL,
+	NULL, NULL, &ftWrite);
 
-    if (lResult == ERROR_SUCCESS) 
-    {
-        do {
+	if (lResult == ERROR_SUCCESS) 
+	{
+		do {
 
-            *lpEnd = TEXT('\0');
-            StringCchCat(lpSubKey, MAX_PATH * 2, szName);
+			*lpEnd = TEXT('\0');
+			StringCchCat(lpSubKey, MAX_PATH * 2, szName);
 
-            if (!RegDelnodeRecurse(hKeyRoot, lpSubKey)) {
-                break;
-            }
+			if (!RegDelnodeRecurse(hKeyRoot, lpSubKey)) {
+				break;
+			}
 
-            dwSize = MAX_PATH;
+			dwSize = MAX_PATH;
 
-            lResult = RegEnumKeyEx(hKey, 0, szName, &dwSize, NULL,
-                                   NULL, NULL, &ftWrite);
+			lResult = RegEnumKeyEx(hKey, 0, szName, &dwSize, NULL,
+			NULL, NULL, &ftWrite);
 
-        } while (lResult == ERROR_SUCCESS);
-    }
+		} while (lResult == ERROR_SUCCESS);
+	}
 
-    lpEnd--;
-    *lpEnd = TEXT('\0');
+	lpEnd--;
+	*lpEnd = TEXT('\0');
 
-    RegCloseKey (hKey);
+	RegCloseKey (hKey);
 
-    // Try again to delete the key.
+	// Try again to delete the key.
 
-    lResult = RegDeleteKey(hKeyRoot, lpSubKey);
+	lResult = RegDeleteKey(hKeyRoot, lpSubKey);
 
-    if (lResult == ERROR_SUCCESS) 
-        return TRUE;
+	if (lResult == ERROR_SUCCESS) 
+	return TRUE;
 
-    return FALSE;
+	return FALSE;
 }
 
 //*************************************************************
@@ -120,10 +120,10 @@ BOOL RegDelnodeRecurse (HKEY hKeyRoot, LPTSTR lpSubKey)
 
 BOOL RegDelnode (HKEY hKeyRoot, LPCTSTR lpSubKey)
 {
-    TCHAR szDelKey[MAX_PATH*2];
+	TCHAR szDelKey[MAX_PATH*2];
 
-    StringCchCopy (szDelKey, MAX_PATH*2, lpSubKey);
-    return RegDelnodeRecurse(hKeyRoot, szDelKey);
+	StringCchCopy (szDelKey, MAX_PATH*2, lpSubKey);
+	return RegDelnodeRecurse(hKeyRoot, szDelKey);
 
 }
 
@@ -180,142 +180,142 @@ For the first file or any subsequent files:
 }
 
 /**
- * The following function shows one way that a GUI application can redirect the stdout 
- * stream to a new console    window. After the following function has been called, the 
- * GUI program can use C runtime functions like printf to display information on the 
- * console window. This can be useful during debugging.
- */
+* The following function shows one way that a GUI application can redirect the stdout 
+* stream to a new console    window. After the following function has been called, the 
+* GUI program can use C runtime functions like printf to display information on the 
+* console window. This can be useful during debugging.
+*/
 
 void RedirectStdOut(void)
 {
-    CONSOLE_SCREEN_BUFFER_INFO csbi;
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
 
-    // Create a new console window.
-    if (!AllocConsole()) return;
+	// Create a new console window.
+	if (!AllocConsole()) return;
 
-    // Set the screen buffer to be larger than normal (this is optional).
-    if (GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi))
-    {
-        csbi.dwSize.Y = 1000; // any useful number of lines...
-        SetConsoleScreenBufferSize(GetStdHandle(STD_OUTPUT_HANDLE), csbi.dwSize);
-    }
- 
+	// Set the screen buffer to be larger than normal (this is optional).
+	if (GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi))
+	{
+		csbi.dwSize.Y = 1000; // any useful number of lines...
+		SetConsoleScreenBufferSize(GetStdHandle(STD_OUTPUT_HANDLE), csbi.dwSize);
+	}
 
-    // Redirect "stdin" to the console window.
-    if (!freopen("CONIN$", "r", stdin)) return;
- 
 
-    // Redirect "stderr" to the console window.
-    if (!freopen("CONOUT$", "w", stderr)) return;
+	// Redirect "stdin" to the console window.
+	if (!freopen("CONIN$", "r", stdin)) return;
 
-    // Redirect "stdout" to the console window.
-    if (!freopen("CONOUT$", "w", stdout)) return;
- 
 
-    // Turn off buffering for "stdout" ("stderr" is unbuffered by default).
+	// Redirect "stderr" to the console window.
+	if (!freopen("CONOUT$", "w", stderr)) return;
 
-    setbuf(stdout, NULL);
+	// Redirect "stdout" to the console window.
+	if (!freopen("CONOUT$", "w", stdout)) return;
+
+
+	// Turn off buffering for "stdout" ("stderr" is unbuffered by default).
+
+	setbuf(stdout, NULL);
 }
 
 int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCommandLine, int cmdShow)
 {
-   BOOL bSuccess;
-   int CMDResult;
-   char choice;
+	BOOL bSuccess;
+	int CMDResult;
+	char choice;
 
-   CMDResult = strcmp(lpszCommandLine, "/h");
-   if (CMDResult == 0)
-   {
-      MessageBox(NULL, "Usage:\n/u - uninstaller friendly interface\n/s - silent mode\n/f - log errors to file\n/l - log errors to console\n/c - console mode (must be combined with /l)\n/h - show help\n", "Information", MB_ICONINFORMATION | MB_OK);
-      return 0;
-   }
+	CMDResult = strcmp(lpszCommandLine, "/h");
+	if (CMDResult == 0)
+	{
+		MessageBox(NULL, "Usage:\n/u - uninstaller friendly interface\n/s - silent mode\n/f - log errors to file\n/l - log errors to console\n/c - console mode (must be combined with /l)\n/h - show help\n", "Information", MB_ICONINFORMATION | MB_OK);
+		return 0;
+	}
 
-   if (strstr(lpszCommandLine, "/l"))
-   {
-      RedirectStdOut();
-   }
+	if (strstr(lpszCommandLine, "/l"))
+	{
+		RedirectStdOut();
+	}
 
-   if (strstr(lpszCommandLine, "/s"))
-   {
-      if (strstr(lpszCommandLine, "/f"))
-      {
-         LogFileOpen("ResetConfig.log");
-      }
-      DeleteEffectPresets();
-      RegDelnode(HKEY_CURRENT_USER, TEXT("Software\\GreenSoftware\\GSPlayer"));
-      if (strstr(lpszCommandLine, "/f"))
-      {
-         LogFileClose();
-      }
-      return 0;
-   }
+	if (strstr(lpszCommandLine, "/s"))
+	{
+		if (strstr(lpszCommandLine, "/f"))
+		{
+			LogFileOpen("ResetConfig.log");
+		}
+		DeleteEffectPresets();
+		RegDelnode(HKEY_CURRENT_USER, TEXT("Software\\GreenSoftware\\GSPlayer"));
+		if (strstr(lpszCommandLine, "/f"))
+		{
+			LogFileClose();
+		}
+		return 0;
+	}
 
-   if (strstr(lpszCommandLine, "/c"))
-   {
-      if (strstr(lpszCommandLine, "/f"))
-      {
-         LogFileOpen("ResetConfig.log");
-      }
-      printf("Are you sure you want to reset the GSPlayer configuration? Y/N\n");
-      scanf("%c", &choice);
-      if (choice == 'y' || choice == 'Y')
-      {
-         DeleteEffectPresets();
-         bSuccess = RegDelnode(HKEY_CURRENT_USER, TEXT("Software\\GreenSoftware\\GSPlayer"));
-         if(bSuccess)
-            printf("Configuration Reset!\n");
-         else
-            printf("There was a problem resetting the configuration.\n");
-      }
-      else if (choice == 'n' || choice == 'N')
-      {
-         printf("Configuration has not been reset.\n");
-      }
-      if (strstr(lpszCommandLine, "/f"))
-      {
-         LogFileClose();
-      }
-      printf("Press any key to exit.\n");
-      getch();
-      return 0;
-   }
+	if (strstr(lpszCommandLine, "/c"))
+	{
+		if (strstr(lpszCommandLine, "/f"))
+		{
+			LogFileOpen("ResetConfig.log");
+		}
+		printf("Are you sure you want to reset the GSPlayer configuration? Y/N\n");
+		scanf("%c", &choice);
+		if (choice == 'y' || choice == 'Y')
+		{
+			DeleteEffectPresets();
+			bSuccess = RegDelnode(HKEY_CURRENT_USER, TEXT("Software\\GreenSoftware\\GSPlayer"));
+			if(bSuccess)
+			printf("Configuration Reset!\n");
+			else
+			printf("There was a problem resetting the configuration.\n");
+		}
+		else if (choice == 'n' || choice == 'N')
+		{
+			printf("Configuration has not been reset.\n");
+		}
+		if (strstr(lpszCommandLine, "/f"))
+		{
+			LogFileClose();
+		}
+		printf("Press any key to exit.\n");
+		getch();
+		return 0;
+	}
 
-   if (strstr(lpszCommandLine, "/u"))
-   {
-      if (MessageBox(NULL, "Do you wish to reset the GSPlayer configuration?", "", MB_ICONQUESTION | MB_YESNO) == IDYES)
-      {
-         if (strstr(lpszCommandLine, "/f"))
-         {
-            LogFileOpen("ResetConfig.log");
-         }
-         DeleteEffectPresets();
-         RegDelnode(HKEY_CURRENT_USER, TEXT("Software\\GreenSoftware\\GSPlayer"));
-         if (strstr(lpszCommandLine, "/f"))
-         {
-            LogFileClose();
-         }
-      }
-      return 0;
-   }
+	if (strstr(lpszCommandLine, "/u"))
+	{
+		if (MessageBox(NULL, "Do you wish to reset the GSPlayer configuration?", "", MB_ICONQUESTION | MB_YESNO) == IDYES)
+		{
+			if (strstr(lpszCommandLine, "/f"))
+			{
+				LogFileOpen("ResetConfig.log");
+			}
+			DeleteEffectPresets();
+			RegDelnode(HKEY_CURRENT_USER, TEXT("Software\\GreenSoftware\\GSPlayer"));
+			if (strstr(lpszCommandLine, "/f"))
+			{
+				LogFileClose();
+			}
+		}
+		return 0;
+	}
 
-   if (MessageBox(NULL, "Are you sure you want to reset the GSPlayer configuration?", "Reset GSPlayer Configuration", MB_ICONQUESTION | MB_YESNO) == IDYES)
-   {
-      if (strstr(lpszCommandLine, "/f"))
-      {
-         LogFileOpen("ResetConfig.log");
-      }
-      DeleteEffectPresets();
-      bSuccess = RegDelnode(HKEY_CURRENT_USER, TEXT("Software\\GreenSoftware\\GSPlayer"));
+	if (MessageBox(NULL, "Are you sure you want to reset the GSPlayer configuration?", "Reset GSPlayer Configuration", MB_ICONQUESTION | MB_YESNO) == IDYES)
+	{
+		if (strstr(lpszCommandLine, "/f"))
+		{
+			LogFileOpen("ResetConfig.log");
+		}
+		DeleteEffectPresets();
+		bSuccess = RegDelnode(HKEY_CURRENT_USER, TEXT("Software\\GreenSoftware\\GSPlayer"));
 
-      if(bSuccess)
-         MessageBox(NULL, "Configuration Reset!", "Success", MB_ICONINFORMATION | MB_OK);
-      else
-         MessageBox(NULL, "There was a problem resetting the configuration.", "Error", MB_ICONERROR | MB_OK);
-      if (strstr(lpszCommandLine, "/f"))
-      {
-         LogFileClose();
-      }
-   }
+		if(bSuccess)
+		MessageBox(NULL, "Configuration Reset!", "Success", MB_ICONINFORMATION | MB_OK);
+		else
+		MessageBox(NULL, "There was a problem resetting the configuration.", "Error", MB_ICONERROR | MB_OK);
+		if (strstr(lpszCommandLine, "/f"))
+		{
+			LogFileClose();
+		}
+	}
 
-   return 0;
+	return 0;
 }
